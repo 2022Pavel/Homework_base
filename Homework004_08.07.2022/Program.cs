788b08b08684bed86d7117a825b7ac5c61dde17b
Pavel <ierpvk@gmail.com>
2022-07-15 09:09:06 +0500

    Homework004_08.07.2022 project

--- Homework004_08.07.2022/Program.cs
+++ Homework004_08.07.2022/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Введите число: ");
 int m = int.Parse(Console.ReadLine());
 for (int i = n; i <= m; i++)
 {
-if (i % 2 == 0) 
+   if (i % 2 == 0) 
    {
       Console.Write(i + " ");
    }
